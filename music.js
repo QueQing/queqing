966c7763d3eeeb23887026d7137ad0fa0e5d04fa
缺氢 <96103241+QueQing@users.noreply.github.com>
2023-11-22 17:36:16 +0800

    Create music.js

---
+++ music.js
@@ -0,0 +1,38 @@
+var music = [
+  {
+    "url": "https://github.com/QueQing/queqing/raw/main/Music/song/Courage%20-%20%E6%88%B8%E6%9D%BE%E9%81%A5.mp3",
+    "lyrics": "https://github.com/QueQing/queqing/raw/9d63b4a92e654f07b2fbd2cc3cf0a1a2a5e454d9/Music/lyrics/Courage%20-%20%E6%88%B8%E6%9D%BE%E9%81%A5.lrc",
+    "songName": "Courage",
+    "singer": "戸松遥"
+  },
+  {
+    "url": "https://raw.githubusercontent.com/QueQing/queqing/9d63b4a92e654f07b2fbd2cc3cf0a1a2a5e454d9/Music/song/Love%20Story%20-%20Taylor%20Swift.mp3",
+    "lyrics": "https://github.com/QueQing/queqing/raw/9d63b4a92e654f07b2fbd2cc3cf0a1a2a5e454d9/Music/lyrics/Love%20Story%20-%20Taylor%20Swift.lrc",
+    "songName": "Love Story",
+    "singer": "Taylor Swift"
+  },
+  {
+    "url": "https://raw.githubusercontent.com/QueQing/queqing/9d63b4a92e654f07b2fbd2cc3cf0a1a2a5e454d9/Music/song/Unravel%20-%20TK%20from%20%E5%87%9B%E3%81%A8%E3%81%97%E3%81%A6%E6%99%82%E9%9B%A8.mp3",
+    "lyrics": "https://github.com/QueQing/queqing/raw/9d63b4a92e654f07b2fbd2cc3cf0a1a2a5e454d9/Music/lyrics/Unravel%20-%20TK%20from%20%E5%87%9B%E3%81%A8%E3%81%97%E3%81%A6%E6%99%82%E9%9B%A8.lrc",
+    "songName": "Unravel",
+    "singer": "TK from 凛として時雨"
+  },
+  {
+    "url": "https://github.com/QueQing/queqing/raw/main/Music/song/%E4%B8%80%E4%B8%AA%E4%BA%BA%E6%83%B3%E7%9D%80%E4%B8%80%E4%B8%AA%E4%BA%BA%20-%20%E6%9B%BE%E6%B2%9B%E6%85%88.mp3",
+    "lyrics": "https://github.com/QueQing/queqing/raw/9d63b4a92e654f07b2fbd2cc3cf0a1a2a5e454d9/Music/lyrics/%E4%B8%80%E4%B8%AA%E4%BA%BA%E6%83%B3%E7%9D%80%E4%B8%80%E4%B8%AA%E4%BA%BA%20-%20%E6%9B%BE%E6%B2%9B%E6%85%88.lrc",
+    "songName": "一个人想着一个人",
+    "singer": "曾沛慈"
+  },
+  {
+    "url": "https://github.com/QueQing/queqing/raw/main/Music/song/%E4%B8%80%E5%90%BB%E5%A4%A9%E8%8D%92%20-%20%E8%83%A1%E6%AD%8C.mp3",
+    "lyrics": "https://github.com/QueQing/queqing/raw/9d63b4a92e654f07b2fbd2cc3cf0a1a2a5e454d9/Music/lyrics/%E4%B8%80%E5%90%BB%E5%A4%A9%E8%8D%92%20-%20%E8%83%A1%E6%AD%8C.lrc",
+    "songName": "一吻天荒",
+    "singer": "胡歌"
+  },
+  {
+    "url": "https://github.com/QueQing/queqing/raw/main/Music/song/%E4%B8%80%E7%AC%91%E5%80%BE%E5%9F%8E%20-%20%E6%B1%AA%E8%8B%8F%E6%B3%B7.mp3",
+    "lyrics": "https://github.com/QueQing/queqing/raw/9d63b4a92e654f07b2fbd2cc3cf0a1a2a5e454d9/Music/lyrics/%E4%B8%80%E7%AC%91%E5%80%BE%E5%9F%8E%20-%20%E6%B1%AA%E8%8B%8F%E6%B3%B7.lrc",
+    "songName": "一笑倾城",
+    "singer": "汪苏泷"
+  }
+]
